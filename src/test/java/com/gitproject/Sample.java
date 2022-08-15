98edd1f25b2403e51f74f28d90e333bcd9a89ae1
nasur89 <nasur2089@gmail.com>
2022-08-15 13:57:03 +0530

    nasur updated codes

--- src/test/java/com/gitproject/Sample.java
+++ src/test/java/com/gitproject/Sample.java
@@ -13,4 +13,16 @@ private void s3() {
 	// TODO Auto-generated method stub
 
 }
+private void n1() {
+	// TODO Auto-generated method stub
+
+}
+private void n2() {
+	// TODO Auto-generated method stub
+
+}
+private void n3() {
+	// TODO Auto-generated method stub
+
+}
 }
