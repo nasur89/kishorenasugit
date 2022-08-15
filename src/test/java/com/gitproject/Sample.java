1d293f4703358964433b6f3e9da06c124c56e4e3
nasur89 <nasur2089@gmail.com>
2022-08-15 12:53:25 +0530

    initial codes

---
+++ src/test/java/com/gitproject/Sample.java
@@ -0,0 +1,16 @@
+package com.gitproject;
+
+public class Sample {
+private void s1() {
+	// TODO Auto-generated method stub
+
+}
+private void s2() {
+	// TODO Auto-generated method stub
+
+}
+private void s3() {
+	// TODO Auto-generated method stub
+
+}
+}
