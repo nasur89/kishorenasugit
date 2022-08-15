998c4d2493e7c6376407c1c2a806db29e5f1b2b0
kishoregitt <kavalankishore@gmail.com>
2022-08-15 13:31:44 +0530

    updated 2 methods by kishore

--- src/test/java/com/gitproject/Sample.java
+++ src/test/java/com/gitproject/Sample.java
@@ -1,16 +1,29 @@
 package com.gitproject;
 
 public class Sample {
-private void s1() {
-	// TODO Auto-generated method stub
+	private void s1() {
+		// TODO Auto-generated method stub
 
-}
-private void s2() {
-	// TODO Auto-generated method stub
+	}
 
-}
-private void s3() {
-	// TODO Auto-generated method stub
+	private void s2() {
+		// TODO Auto-generated method stub
 
-}
+	}
+
+	private void s3() {
+		// TODO Auto-generated method stub
+
+ 	}
+	private void s4() {
+		System.out.println("kishore added method 4");
+	}
+	private void s5() {
+		System.out.println("kishore added method 5");
+
+	}
+	private void s6() {
+		System.out.println("kishore added method 6");
+
+	}
 }
